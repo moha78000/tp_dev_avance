e096ed8dae2cae638cf2148055bd5392d6147478
Khaldi Mohamed <moh78078000@gmail.com>
2026-02-21 00:20:31 +0100

    Correction lors de la recuperation du role (0 par default)

--- controllers/navControler.js
+++ controllers/navControler.js
@@ -7,6 +7,6 @@ exports.getAccueil = (req, res, next) => {
     res.render('accueil', {
         pageTitle: 'Accueil',
         user: req.session.isLog ? { loggedIn: true } : { loggedIn: false },
-        role: req.session.role || null
+        role: req.session.role ?? 0
     });
 };
