1d7f00af885d62e321655a87c6dd61dd274c7797
Khaldi Mohamed <moh78078000@gmail.com>
2026-02-21 13:26:30 +0100

    Nettoyage des controllers appeles

--- routes/client.js
+++ routes/client.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const isAuth = require("../middleware/auth-midd");
-const adminData = require("./admin");
+const {isAuth} = require("../middleware/auth-midd");
+//const adminData = require("./admin");
 const justePrix = require("../controllers/justePrix");
 const navControler = require('../controllers/navControler');
 const logout = require('../controllers/logout');
-const {produits} = require("./admin");
+//const {produits} = require("./admin");
 
-router.get('/justePrix', isAuth, justePrix.getJustePrix);
 router.get("/", navControler.getAccueil);
-router.get('/out', isAuth, logout.getLogout);;
 
 module.exports = router;
