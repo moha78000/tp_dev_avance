d4035b547bb039277318b08667443393ae5072b2
Khaldi Mohamed <moh78078000@gmail.com>
2026-02-21 14:53:29 +0100

    Ajout des roles possibles 0 ou 1

--- model/User.js
+++ model/User.js
@@ -3,7 +3,7 @@ const mongoose = require('../outils/database');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: Number, default: 0 } // 0=utilisateur normal, 1=admin web, 2=admin système
+    role: { type: Number, enum: [0, 1], default: 0 } // 0=utilisateur normal, 1=admin web
 }, { timestamps: true });
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
