443b816a148e69a9a61d717183e8a85267ad6900
Khaldi Mohamed <moh78078000@gmail.com>
2026-02-24 12:33:46 +0100

    Ajout vérification si bd ok VERSION FINALE DU PROJET

--- outils/database.js
+++ outils/database.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const uri = 'mongodb://22302683:u5sytrv9@192.168.24.1:27017/22302683_db';
 
-mongoose.connect(uri);
+mongoose.connect(uri)
+    .then(() => console.log("MongoDB connecté"))
+    .catch(err => console.error(err));
 
 module.exports = mongoose;
